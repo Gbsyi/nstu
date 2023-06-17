301ddbd5502a146eb875e658f0789f920676a976
Vladislav Yakovlev <79984303+Gbsyi@users.noreply.github.com>
2023-06-17 14:50:45 +0700

    4

--- Lab3v7/Lab3v7.cpp
+++ Lab3v7/Lab3v7.cpp
@@ -60,56 +60,60 @@ int get_max_tree_len(const TreeNode* node)
                : max_right_len;
 }
 
-void print_Tree(TreeNode* p, int level)
+void print_tree(TreeNode* p, int level)
 {
     if (p)
     {
-        print_Tree(p->left, level + 1);
+        print_tree(p->left, level + 1);
         for (int i = 0; i < level; i++) cout << "   ";
         cout << p->value << endl;
-        print_Tree(p->right, level + 1);
+        print_tree(p->right, level + 1);
     }
 }
 
 int main()
 {
     auto root = TreeNode{
-        1,
+        0,
         new TreeNode{
-            3,
+            1,
             new TreeNode{
-                4,
-                nullptr,
-                nullptr
-            },
-            new TreeNode{
-                5,
-                new TreeNode{
-                    8,
+                3, new TreeNode{
+                    7,
                     nullptr,
                     nullptr
                 },
                 new TreeNode{
-                    6,
+                    8,
                     nullptr,
                     nullptr
                 }
+            },
+            new TreeNode{
+                4,
+                nullptr,
+                nullptr
+
             }
         },
         new TreeNode{
             2,
             new TreeNode{
-                7,
+                5,
                 nullptr,
                 nullptr
             },
-            nullptr
+            new TreeNode{
+                6,
+                nullptr,
+                nullptr
+            },
         }
     };
 
     cout << "" << get_max_tree_len(&root) << endl;
 
-    print_Tree(&root, 0);
+    print_tree(&root, 0);
 
     return 0;
 }
--- Lab5v7/Lab5v7.cpp
+++ Lab5v7/Lab5v7.cpp
@@ -3,105 +3,118 @@
 
 using namespace std;
 
+const std::string PRODUCT_OVERHEATED = "Перегрет";
+const std::string PRODUCT_FROZEN = "Переморожен";
+const std::string PRODUCT_NORMAL = "Нормальное";
+
 class Product
 {
-    string name;
-    double mass;
-    double temperature;
-    double maxTemperature;
-    double minTemperature;
-    string state;
-    double heatCapacity;
+    int id_;
+    std::string name_;
+    double mass_;
+    double temperature_;
+    double max_temperature_;
+    double min_temperature_;
+    std::string state_;
+    double heat_capacity_;
+
+    void set_temperature(double temperature)
+    {
+        temperature_ = temperature;
+
+        if (temperature_ >= max_temperature_)
+        {
+            state_ = PRODUCT_OVERHEATED;
+        }
+        else if (temperature_ <= min_temperature_)
+        {
+            state_ = PRODUCT_FROZEN;
+        }
+    }
 
 public:
-    Product(string productName, double productMass, double productTemperature, double productMaxTemp,
+    Product(int id, std::string productName, double productMass, double productTemperature, double productMaxTemp,
             double productMinTemp, double productHeatCapacity)
-        : name(std::move(productName)), mass(productMass), temperature(productTemperature),
-          maxTemperature(productMaxTemp),
-          minTemperature(productMinTemp), heatCapacity(productHeatCapacity)
+        : id_(id), name_(std::move(productName)), mass_(productMass), temperature_(productTemperature),
+          max_temperature_(productMaxTemp),
+          min_temperature_(productMinTemp), heat_capacity_(productHeatCapacity)
     {
-        state = "Нормальное";
+        state_ = PRODUCT_NORMAL;
     }
 
-    string getName() const
+    int get_id() const
     {
-        return name;
+        return id_;
     }
 
-    double getMass() const
+    std::string get_name() const
     {
-        return mass;
+        return name_;
     }
 
-    double getTemperature() const
+    double get_weight() const
     {
-        return temperature;
+        return mass_;
     }
 
-    double getMaxTemperature() const
+    double get_temperature() const
     {
-        return maxTemperature;
+        return temperature_;
     }
 
-    double getMinTemperature() const
+    double get_max_temperature() const
     {
-        return minTemperature;
+        return max_temperature_;
     }
 
-    std::string getState() const
+    double get_min_temperature() const
     {
-        return state;
+        return min_temperature_;
     }
 
-    double getHeatCapacity() const
+    std::string get_state() const
     {
-        return heatCapacity;
+        return state_;
     }
 
-    void transferHeatEnergy(double heatEnergy)
+    double get_heat_capacity() const
     {
-        temperature += heatEnergy / heatCapacity;
-
-        if (temperature >= maxTemperature)
-        {
-            state = "Перегрет";
-        }
-        else if (temperature <= minTemperature)
-        {
-            state = "Переморожен";
-        }
-        else
-        {
-            state = "Нормальное";
-        }
+        return heat_capacity_;
     }
 
-    double getFinalTemperature(double heatEnergy) const
+    void transfer_heat_energy(double heatEnergy)
     {
-        return temperature + heatEnergy / heatCapacity;
+        const auto temperature = temperature_ + heatEnergy / heat_capacity_;
+        set_temperature(temperature);
+    }
+
+    double get_final_temperature(double heatEnergy) const
+    {
+        return temperature_ + heatEnergy / heat_capacity_;
     }
 };
+
 
 int main()
 {
     // Пример использования класса Product
-    Product product("Молоко", 1.5, 20.0, 30.0, 0.0, 1.0);
+    Product product(1, "Молоко", 1.5, 20.0, 30.0, 0.0, 1.0);
 
-    cout << "Название продукта: " << product.getName() << endl;
-    cout << "Масса продукта: " << product.getMass() << " кг" << endl;
-    cout << "Текущая температура продукта: " << product.getTemperature() << " °C" << endl;
-    cout << "Максимально допустимая температура: " << product.getMaxTemperature() << " °C" << endl;
-    cout << "Минимально допустимая температура: " << product.getMinTemperature() << " °C" << endl;
-    cout << "Состояние продукта: " << product.getState() << endl;
-    cout << "Теплоемкость продукта: " << product.getHeatCapacity() << " Дж/°C" << endl;
+    cout << "Название продукта: " << product.get_name() << endl;
+    cout << "Масса продукта: " << product.get_weight() << " кг" << endl;
+    cout << "Текущая температура продукта: " << product.get_temperature() << " °C" << endl;
+    cout << "Максимально допустимая температура: " << product.get_max_temperature() << " °C" << endl;
+    cout << "Минимально допустимая температура: " << product.get_min_temperature() << " °C" << endl;
+    cout << "Состояние продукта: " << product.get_state() << endl;
+    cout << "Теплоемкость продукта: " << product.get_heat_capacity() << " Дж/°C" << endl;
 
     double heatEnergy = 500.0;
-    product.transferHeatEnergy(heatEnergy);
-    cout << "Новая температура продукта после передачи " << heatEnergy << " Дж: " << product.getTemperature() << " °C"
+    product.transfer_heat_energy(heatEnergy);
+    cout << "Новая температура продукта после передачи " << heatEnergy << " Дж: " << product.get_temperature() << " °C"
         << endl;
 
     double additionalHeatEnergy = 200.0;
-    double finalTemperature = product.getFinalTemperature(additionalHeatEnergy);
+    double finalTemperature = product.get_final_temperature(additionalHeatEnergy);
     cout << "Температура продукта при передаче дополнительной энергии " << additionalHeatEnergy << " Дж: " <<
         finalTemperature << " °C" << endl;
 
